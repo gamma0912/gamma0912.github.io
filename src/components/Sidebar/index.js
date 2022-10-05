93ca183ce320811787725522dd56b3ee336af497
gamma0912 <gamma0912@outlook.com>
2022-10-04 18:25:24 -0700

    bleh

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png' //TODO: change this to own logo
 import LogoSubtitle from '../../assets/images/logo_sub.png' //TODO: change this to own name
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className = 'nav-bar'>
@@ -9,6 +11,17 @@ const Sidebar = () => (
             <img src = {LogoS} alt = "logo" />
             <img className = "sub-logo" src = {LogoSubtitle} alt = "slobodan" />
         </Link>
+        <nav>
+            <NavLink exact = "true" activeclassname = "active" to = "/">
+                <FontAwesomeIcon icon = {faHome} color = "#4d4d4e" /> 
+            </NavLink>
+            <NavLink exact = "true" activeclassname = "active" className = "about-link" to = "/about">
+                <FontAwesomeIcon icon = {faUser} color = "#4d4d4e" /> 
+            </NavLink>
+            <NavLink exact = "true" activeclassname = "active" className = "contact-link" to = "/contact">
+                <FontAwesomeIcon icon = {faEnvelope} color = "#4d4d4e" /> 
+            </NavLink>
+        </nav>
     </div>
 )
 
