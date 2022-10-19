ccc4ba7b5acb944bd98bf174899bfd65b6f51298
gamma0912 <gamma0912@outlook.com>
2022-10-18 21:48:27 -0700

    homepage text animations

---
+++ src/components/Home/index.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import LogoTitle from "../../assets/images/logo-s.png";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss"
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const greetingArray = "Hi,".split("")
+    const imaArray = "I'm a".split("")
+    const nameArray = "ussy".split("")
+    const jobArray = "comp sci student.".split("")
+    //use the idx values to adjust appearance delay in milliseconds
+    return (
+        <div className = "container home-page">
+            <div className = "text-zone">
+                <h1>
+                <AnimatedLetters letterClass = {letterClass}
+                strArray = {greetingArray}
+                idx = {12}/> 
+                <br />
+                <AnimatedLetters letterClass = {letterClass}
+                strArray = {imaArray}
+                idx = {15}/>
+                <img src = {LogoTitle} alt = "developer" />
+                <AnimatedLetters letterClass = {letterClass}
+                strArray = {nameArray}
+                idx = {22}/>
+                <br />
+                <AnimatedLetters letterClass = {letterClass}
+                strArray = {jobArray}
+                idx = {26}/>
+                </h1>
+                <h2>
+                Looking for software internships!
+                </h2>
+                <Link to = "/contact" className = "flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    );
+
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png' //TODO: change this to own logo
-import LogoSubtitle from '../../assets/images/logo_sub.png' //TODO: change this to own name
+import LogoSubtitle from '../../assets/images/logo_sub_gamma.png' //TODO: change this to own name
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
